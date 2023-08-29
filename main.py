22aa8dcb674e83d463b851483b6cdc1a5c846a50
xroi <40072297+xroi@users.noreply.github.com>
2023-08-29 14:07:37 +0300

    Added Auto Correlations Calculations

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import argparse
 import h5py
 import numpy as np
+import scipy as scipy
 from PIL import Image, ImageSequence
+import height_funcs
+import pandas as pd
 
 """
 todo:
@@ -132,32 +135,11 @@ def get_combined_density_map(time, args):
     return combined_density_map
 
 
-def ball_average(x, y, z, arr, r):
-    # function written by AI
-    x_min = max(0, x - r)
-    x_max = min(arr.shape[0], x + r + 1)
-    y_min = max(0, y - r)
-    y_max = min(arr.shape[1], y + r + 1)
-    z_min = max(0, z - r)
-    z_max = min(arr.shape[2], z + r + 1)
-    sub_arr = arr[x_min:x_max, y_min:y_max, z_min:z_max]
-    xx, yy, zz = np.mgrid[:sub_arr.shape[0], :sub_arr.shape[1], :sub_arr.shape[2]]
-    mask = ((xx - (x - x_min)) ** 2 + (yy - (y - y_min)) ** 2 + (zz - (z - z_min)) ** 2) <= r ** 2
-    return np.mean(sub_arr[mask])
-
-
-def get_single_pixel_height(x, y, combined_density_map, args):
-    for z in range(combined_density_map.shape[2] - args["needle_radius_px"], -1, -1):
-        if ball_average(x, y, z, combined_density_map, args["needle_radius_px"]) > args["needle_threshold"]:
-            return z / combined_density_map.shape[2]
-    return 0
-
-
 def get_height_map(combined_density_map, args):
     height_map = np.zeros(shape=combined_density_map.shape[:2])
     for x in range(combined_density_map.shape[0]):
         for y in range(combined_density_map.shape[1]):
-            height_map[x, y] = get_single_pixel_height(x, y, combined_density_map, args)
+            height_map[x, y] = height_funcs.get_single_pixel_height_old(x, y, combined_density_map, args)
     return height_map
 
 
@@ -171,13 +153,31 @@ def main():
 
     maps = []
     for i in range(args["interval_ns"], args["simulation_time_ns"], args["interval_ns"]):
+        print(i)
         combined_density_map = get_combined_density_map(i, args)
         height_map = get_height_map(combined_density_map, args)
         maps.append(height_map)
+    acorrs = temporal_auto_correlate(maps)
     if args["output_gif"]:
         output_gif(args, maps)
     if args["output_hdf5"]:
         output_hdf5(maps)
+
+
+def temporal_auto_correlate(maps):
+    """
+    Calculates the temporal auto correlation of each pixel with itself over different time lags.
+    """
+    stacked_maps = np.dstack(maps)
+    temporal_auto_correlations = np.zeros(stacked_maps.shape)
+    for x in range(stacked_maps.shape[0]):
+        for y in range(stacked_maps.shape[1]):
+            pixel_time_series = pd.Series(stacked_maps[x, y,])
+            auto_corrs = []
+            for delta_t in range(stacked_maps.shape[2] - 1):
+                auto_corrs.append(pixel_time_series.autocorr(lag=delta_t))
+            temporal_auto_correlations[x, y,] = np.array(auto_corrs)
+    return temporal_auto_correlations
 
 
 def output_gif(args, maps):
@@ -186,7 +186,7 @@ def output_gif(args, maps):
         im = Image.fromarray((height_map * 255).astype(np.uint8)).resize(
             (args["output_resolution_x"], args["output_resolution_y"]), resample=Image.BOX)
         images.append(im)
-    images[0].save(args["output_path"], append_images=images[1:], save_all=True, duration=100, loop=0)
+    images[0].save(args["output_gif_path"], append_images=images[1:], save_all=True, duration=100, loop=0)
 
 
 if __name__ == "__main__":

3645ba2e760911ef78c87dc0249382749ab73874
xroi <40072297+xroi@users.noreply.github.com>
2023-10-15 17:11:45 +0300

    updated local concate rmf script to take paramenters

--- scripts/LOCAL_concat_rmf.sh
+++ scripts/LOCAL_concat_rmf.sh
@@ -4,5 +4,10 @@ set IMP_FOLDER=/cs/labs/ravehb/ravehb/imp/fast_conda/
 set IMP=$IMP_FOLDER/setup_environment.sh
 source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate.csh
 
+if ($#argv != 5) then
+    echo "Syntax: $0 <folder_input_path> <rmf_output_path> <start_time> <end_time> <interval>"
+    exit 0
+endif
+
 #$IMP python3 ../scripts/concat_rmf.py $argv:q
-$IMP python3 ../scripts/concat_rmf.py --input-path ../../08-10-2023-NTR/ --output-path new.rmf --start-time-ns 1000 --end-time-ns 10000 --interval-ns 1000
+$IMP python3 ../scripts/concat_rmf.py --input-path $1 --output-path $2 --start-time-ns $3 --end-time-ns $4 --interval-ns $5

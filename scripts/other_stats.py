c5702da188bb2c2964d6125400c7fad2fd3c6a1f
xroi <40072297+xroi@users.noreply.github.com>
2023-10-16 09:04:26 +0300

    added a script to read addtional stats

---
+++ scripts/other_stats.py
@@ -0,0 +1,28 @@
+import argparse
+from IMP.npctransport import *
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-path", type=str, required=True, help="Path to folder")
+    parser.add_argument("--output-path", type=str, required=True)
+    return vars(parser.parse_args())
+
+
+def main():
+    args = parse_arguments()
+    with open(args["input_path"], "rb") as f:
+        output = Output()
+        fstring = f.read()
+        output.ParseFromString(fstring)
+        time_ns_arr = []
+        energy_arr = []
+        for i in range(len(output.statistics.global_order_parmas)):
+            time_ns_arr.append(output.statistics.global_order_parmas[i].time_ns)
+            energy_arr.append(output.statistics.global_order_parmas[i].energy)
+        print(time_ns_arr)
+        print(energy_arr)
+
+
+if __name__ == "__main__":
+    main()

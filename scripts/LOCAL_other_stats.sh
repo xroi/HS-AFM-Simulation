212bcd4a1cb9e4d6e3ffe55d876701870ba134ba
xroi <40072297+xroi@users.noreply.github.com>
2023-10-16 09:05:43 +0300

    added a local script to read addtional stats

---
+++ scripts/LOCAL_other_stats.sh
@@ -0,0 +1,15 @@
+#!/bin/csh -f
+
+if ($#argv != 1) then
+    echo "Syntax: $0 <pb_file_path>"
+    exit 0
+endif
+
+# Setup Environment
+set IMP_FOLDER=/cs/labs/ravehb/ravehb/imp/fast_conda/
+set IMP=$IMP_FOLDER/setup_environment.sh
+source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate.csh
+set SCRIPT=`readlink -f "$0"`
+set DIRNAME=`dirname "$SCRIPT"`
+
+$IMP python3 $DIRNAME/other_stats.py $1

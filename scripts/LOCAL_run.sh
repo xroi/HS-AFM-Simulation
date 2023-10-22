7b763ed71edf1dfa594dd9b4dbde55da920cb3ad
xroi <40072297+xroi@users.noreply.github.com>
2023-10-22 13:45:51 +0300

    fixed script bug

--- scripts/LOCAL_run.sh
+++ scripts/LOCAL_run.sh
@@ -16,11 +16,12 @@ source /cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/ven
 set INPUT_PATH=$1/
 set OUTPUT_PATH=$2
 set ORIGINAL_ARGS_PATH=$3
-set ARGS_PATH=$OUTPUT_PATH._TEMP_ARGS.txt
+set ARGS_PATH=$OUTPUT_PATH._ARGS.txt
 echo input path is $INPUT_PATH
 echo output path is $OUTPUT_PATH
 
 cp $ORIGINAL_ARGS_PATH $ARGS_PATH
+echo "" >> $ARGS_PATH
 echo "--input-path $INPUT_PATH" >> $ARGS_PATH
 echo "--output-path-prefix $OUTPUT_PATH" >> $ARGS_PATH
 

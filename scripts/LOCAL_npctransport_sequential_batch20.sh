93a444e47a52d1a0ca087df0cc5c409a0f2767e8
xroi <40072297+xroi@users.noreply.github.com>
2023-10-22 15:50:57 +0300

    added & fixed batch npctransport scripts

--- scripts/LOCAL_npctransport_sequential_batch20.sh
+++ scripts/LOCAL_npctransport_sequential_batch20.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=7-0
 #SBATCH --mail-user=roi.eliasian@mail.huji.ac.il
 #SBATCH --mail-type=ALL
-#SBATCH --array=0-10
+#SBATCH --array=0-20
 
 if ($#argv != 2) then
     echo "Syntax: $0 <output_folder_path> <config_path>"
@@ -12,9 +12,7 @@ endif
 
 set OUTPUT_PATH=$1/
 set CONFIG_PATH=$2
-set SCRIPT=`readlink -f "$0"`
-set DIRNAME=`dirname "$SCRIPT"`
 
 
 mkdir -p $OUTPUT_PATH
-$DIRNAME/LOCAL_npctransport_sequential.sh 1 1000 1000 $OUTPUT_PATH/$SLURM_ARRAY_TASK_ID $CONFIG_PATH
+/cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/LOCAL_npctransport_sequential.sh 1 1000 1000 $OUTPUT_PATH/$SLURM_ARRAY_TASK_ID $CONFIG_PATH

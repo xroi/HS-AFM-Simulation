ad650925785c0d7345718b2889badaa3beebe50c
xroi <40072297+xroi@users.noreply.github.com>
2023-10-24 12:36:03 +0300

    updated scripts

--- scripts/LOCAL_npctransport_sequential_batch50.sh
+++ scripts/LOCAL_npctransport_sequential_batch50.sh
@@ -1,18 +1,38 @@
-#!/bin/csh -f
+#!/bin/bash
 #SBATCH --mem=2g
 #SBATCH --time=7-0
-#SBATCH --mail-type=ALL
 #SBATCH --array=0-50
+#SBATCH --killable
+#SBATCH --requeue
+#SBATCH -c2
 
+SECOND_JOB_OFFSET=1000
 
-if ($#argv != 2) then
-    echo "Syntax: $0 <output_folder_path> <config_path>"
-    exit 0
-endif
+if [ $# != 2 ]; then
+  echo "Syntax: $0 <output_folder_path> <config_path>"
+  exit 1
+fi
 
-set OUTPUT_PATH=$1/
-set CONFIG_PATH=$2
-
-
+OUTPUT_PATH=${1}/
+CONFIG_PATH=${2}
 mkdir -p $OUTPUT_PATH
-/cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/LOCAL_npctransport_sequential.sh 1 1000 1000 $OUTPUT_PATH/$SLURM_ARRAY_TASK_ID $CONFIG_PATH
+
+declare -a IDs=()
+IDs+=(${SLURM_ARRAY_TASK_ID})
+IDs+=($((SLURM_ARRAY_TASK_ID + SECOND_JOB_OFFSET)))
+echo IDs: ${IDs[@]}
+
+echo "Running jobs"
+declare -a PIDs=()
+for ID in ${IDs[@]}; do
+  /cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/LOCAL_npctransport_sequential.sh 1 1000 1000 $OUTPUT_PATH/${ID} $CONFIG_PATH
+  PID=$!
+  PIDs+=($PID)
+  echo $PID submitted, workid $ID
+done
+
+echo "Waiting for all jobs to finish"
+for PID in "${PIDs[@]}"; do
+  wait "$PID"
+  echo "$PID finished"
+done

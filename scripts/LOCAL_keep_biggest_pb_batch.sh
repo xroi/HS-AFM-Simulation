25dd7f60ec2923c2e8e51a275bc9997e6925bd45
xroi <40072297+xroi@users.noreply.github.com>
2023-10-26 09:57:26 +0300

    added script to keep only latest 2 pb

---
+++ scripts/LOCAL_keep_biggest_pb_batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+  echo "Syntax: $0 <dir_path> <amount>"
+  exit 1
+fi
+
+for i in (( i=0; i<=${2}; i++ ))
+do
+   tree -fQFi --sort=size ${1}/${i}/ | grep \.pb\"$ | tail +3 | xargs rm
+done

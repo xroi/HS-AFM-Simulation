d11edc19233ebad9e200b9f99222070caaf2780d
xroi <40072297+xroi@users.noreply.github.com>
2023-10-31 17:05:47 +0200

    updated scripts

--- scripts/npctransport/LOCAL_replace_old_pb.sh
+++ scripts/npctransport/LOCAL_replace_old_pb.sh
@@ -2,7 +2,7 @@
 
 set IMP_FOLDER=/cs/labs/ravehb/ravehb/imp/fast_conda/
 set IMP=$IMP_FOLDER/setup_environment.sh
-source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate.csh
+source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate
 
 if [ $# != 1 ]; then
   echo "Syntax: $0 <path>"

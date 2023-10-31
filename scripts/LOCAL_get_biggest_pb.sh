fb825adc5b1b1b8612c25f4f09cea01d7566c97d
xroi <40072297+xroi@users.noreply.github.com>
2023-10-31 18:13:14 +0200

    updated scripts

--- scripts/LOCAL_get_biggest_pb.sh
+++ scripts/LOCAL_get_biggest_pb.sh
@@ -7,6 +7,6 @@ fi
 
 #tree -fQFi --sort=size ${1}/ | grep \.pb\"$ | tail +2 | cut -d '.' -f 1
 
-BIGGEST_PB_N=$(/cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/LOCAL_get_biggest_pb_n.sh $OUTPUT_PATH/${ID})
+BIGGEST_PB_N=$(/cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/LOCAL_get_biggest_pb_n.sh ${1})
 BIGGEST_PB="${BIGGEST_PB_N}.pb"
 echo ${BIGGEST_PB}

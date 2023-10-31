fec1911be7754e6bb21286b590240b88d39b8510
xroi <40072297+xroi@users.noreply.github.com>
2023-10-31 17:10:35 +0200

    updated scripts

--- scripts/npctransport/LOCAL_replace_old_pb.sh
+++ scripts/npctransport/LOCAL_replace_old_pb.sh
@@ -1,6 +1,6 @@
 #!/bin/csh
 
-set IMP_FOLDER=/cs/labs/ravehb/ravehb/imp/fast_conda/
+set IMP_FOLDER=/cs/labs/ravehb/ravehb/imp/fast_conda2/
 set IMP=$IMP_FOLDER/setup_environment.sh
 source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate.csh
 

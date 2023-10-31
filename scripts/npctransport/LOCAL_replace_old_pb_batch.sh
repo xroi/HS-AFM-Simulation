e95ab0128515a097559c46d875207d8929518a1e
xroi <40072297+xroi@users.noreply.github.com>
2023-10-31 16:57:20 +0200

    updated scripts

--- scripts/npctransport/LOCAL_replace_old_pb_batch.sh
+++ scripts/npctransport/LOCAL_replace_old_pb_batch.sh
@@ -9,6 +9,6 @@ fi
 
 for (( i=1 ; i<=${2} ; i++ ));
 do
-  $BIGGEST_PB = $(/cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/LOCAL_get_biggest_pb.sh $OUTPUT_PATH/${ID})
+  BIGGEST_PB = $(/cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/LOCAL_get_biggest_pb.sh $OUTPUT_PATH/${ID})
   /cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/npctransport/LOCAL_replace_old_pb.sh $BIGGEST_PB
 done

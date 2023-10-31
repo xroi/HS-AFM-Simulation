8b826ef9c22c4db1fc90000177b0be6654c104fe
xroi <40072297+xroi@users.noreply.github.com>
2023-10-31 17:08:30 +0200

    updated scripts

--- scripts/npctransport/LOCAL_replace_old_pb.sh
+++ scripts/npctransport/LOCAL_replace_old_pb.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/csh
 
 set IMP_FOLDER=/cs/labs/ravehb/ravehb/imp/fast_conda/
 set IMP=$IMP_FOLDER/setup_environment.sh
-source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate
+source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate.csh
 
-if [ $# != 1 ]; then
-  echo "Syntax: $0 <path>"
-  exit 1
-fi
+if ($#argv != 1) then
+    echo "Syntax: $0 <path>"
+    exit 1
+endif
 
-$IMP python3 /cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/npctransport/edit_old_pb.py ${1} TEMP_replace_old_pb.pb
-cp TEMP_replace_old_pb.pb ${1}
+$IMP python3 /cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/npctransport/edit_old_pb.py $1 TEMP_replace_old_pb.pb
+cp TEMP_replace_old_pb.pb $1
 rm TEMP_replace_old_pb.pb

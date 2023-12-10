05c3e14ab7532fd0f1fa7b41c7f79e8713b119a6
xroi <40072297+xroi@users.noreply.github.com>
2023-12-10 09:32:23 +0200

    updated add cylinders script

--- scripts/LOCAL_add_cylinders_to_rmf.sh
+++ scripts/LOCAL_add_cylinders_to_rmf.sh
@@ -1,12 +1,12 @@
 #!/bin/csh -f
 set IMP_FOLDER=/cs/labs/ravehb/ravehb/imp/fast_conda/
 set IMP=$IMP_FOLDER/setup_environment.sh
-set SCR=/cs/labs/ravehb/ravehb/imp/repository_editing/modules/npctransport/utility/add_cylinder.py
+set SCR=/cs/labs/ravehb/roi.eliasian/NpcTransportExperiment/HS-AFM-Simulation/scripts/NPC_rmf_to_pdb_v3.py
 source /cs/labs/ravehb/ravehb/External/venv_imp2023_v2/bin/activate.csh
 
 if ($#argv != 3) then
-    echo "Syntax: $0 <rmf_input_path> <pb_input_path> <rmf_output_path>"
+    echo "Syntax: $0 <rmf_input_path> <pb_input_path> <pdb_output_path>"
     exit 0
 endif
 
-$IMP python3 $SCR --input_rmf $1 --ref_output $2 --output_rmf $3
+$IMP python3 $SCR --input_rmf $1 --ref_output $2 --output_pdb $3
